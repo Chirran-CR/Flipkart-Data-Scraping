d1b3c6b59259dea80a3cf2bbf733d5b4da4e5acc
Chirran <swainchittaranjan345@gmail.com>
2023-08-13 17:24:34 +0530

    server is setup and db is connected

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+
+const { notFound, errorHandler } = require("./controllers/errorController.js");
+const dbConnect = require("./config/dbConnect.js");
+
+//configurattion of env. variables
+dotenv.config({path:"./.env"});
+
+//connect DB
+dbConnect();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 8080;
+
+//api
+app.use("/",(req,res)=>{
+    res.send({
+        message:"Welcome to the API to scrape flipkart data",
+        listOfAPIs:{
+            "createAUser(post)": "user/register =>`to register a new user`",
+            "login User(post)": "user/login =>`to login a user`",
+            "scrape flipkart data(post)": "user/scrapedata =>`to scrape the data and store it into DB`",
+            "get scraped data(get)": "user/scrapedata =>`to get the already stored scraped data from DB`"
+        }
+    })
+})
+//for page not found error
+app.use(notFound);
+//for error in any controller of route
+app.use(errorHandler);
+
+//create Server
+app.listen(PORT,()=>{
+    console.log(`Server is listening at port: ${PORT}`);
+});
